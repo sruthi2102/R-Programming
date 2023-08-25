1af76e60dab0cbecb059afe56e9b02bd86f9fed5
sruthi2102 <122351942+sruthi2102@users.noreply.github.com>
2023-08-25 14:12:13 +0530

    Create ex 1.R

---
+++ ex 1.R
@@ -0,0 +1,6 @@
+name <- readline("Enter your name: ")
+age <- as.numeric(readline("Enter your age: "))
+cat("Name: ", name, "\n")
+cat("Age: ", age, "\n")
+cat("R Version: ", R.version.string, "\n")
+
